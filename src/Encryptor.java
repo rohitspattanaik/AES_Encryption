2524f2263e4c1afc12ff9d34b8c852a574627b44
rohit <rohit.pattanaik@gmail.com>
2014-11-02 14:11:38 -0600

    before pulling

--- src/Encryptor.java
+++ src/Encryptor.java
@@ -1,4 +1,6 @@
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 /**
  * Created by rsp615 on 10/28/14.
@@ -10,6 +12,7 @@ public class Encryptor {
 
     private File inputFile;
     private String input;
+    private Scanner scanner;
 
     private int plainTextMatrix[][] = new int[4][4]; //make private
     private int keyMatrix[][] = new int[4][4];
@@ -23,10 +26,27 @@ public class Encryptor {
         }
     }
 
-    private void readFiles() {
-        keyFile = new File(key);
+    public void printKey() {
+        for(int x = 0; x < 4; x++) {
+            for(int y = 0; y < 4; y++) {
+                System.out.print(Integer.toHexString(keyMatrix[x][y]) + " ");
+            }
+            System.out.println();
+        }
+    }
 
+    private boolean readFiles() {
+        keyFile = new File(key);
         inputFile = new File(input);
+
+        try {
+            scanner = new Scanner(inputFile);
+        }
+        catch(FileNotFoundException e) {
+            System.out.println("Error: Input File Not Found");
+            return false;
+        }
+        return true;
     }
 
     public void addToMatrix(String inputLine) {
@@ -34,11 +54,9 @@ public class Encryptor {
         if(inputLine.length() < 32) {
             int diff = 32 - (int)inputLine.length();
             String tempPad = "0";
-
             for(int i = 0; i < diff; i++) {
                 tempPad = tempPad + "0";
             }
-
             inputLine = inputLine + tempPad;
         }
 
@@ -51,15 +69,26 @@ public class Encryptor {
         //The pattern to add elements in column major
         for(int y = 0; y < 4; y++) {
             for(int x = 0; x < 4; x++) {
-                //System.out.println(inputLine.substring(0,2));
                 plainTextMatrix[x][y] = Integer.parseInt(inputLine.substring(0,2), 16);
-                //System.out.println(plainTextMatrix[x][y]);
                 if(inputLine.length() != 0) {
                     inputLine = inputLine.substring(2);
                 }
 
             }
         }
+    }
+
+    public boolean addFiles(String inputFile, String keyFile) {
+        input = inputFile;
+        key = keyFile;
+        if(!readFiles()) {
+           return false;
+        }
+        return true;
+    }
+
+    public void encrypt() {
+
     }
 
 
