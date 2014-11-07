545bf66d7c1e73f6c5bf0e68ec7aa8757f2f3cab
rohit <rohit.pattanaik@gmail.com>
2014-11-06 18:03:23 -0600

    continuing decryptor. no tests yet

--- src/Decryptor.java
+++ src/Decryptor.java
@@ -1,5 +1,4 @@
-import java.io.BufferedWriter;
-import java.io.File;
+import java.io.*;
 import java.util.Scanner;
 
 
@@ -19,4 +18,210 @@ public class Decryptor {
     private int roundKeyMatrix[][] = new int[4][44]; //collection of roundKeys for all rounds.
 
     private static final int nk = 4; //constant for 128 bit implementation
+
+    /*Following print methods useful for debugging and keeping track of progress of encryption*/
+    public void printState() {
+        for(int x = 0; x < 4; x++) {
+            for(int y = 0; y < 4; y++) {
+                String temp = Integer.toHexString(cipherTextMatrix[x][y]);
+                if(temp.length() < 2) {
+                    temp = "0" + temp;
+                }
+                System.out.print(temp + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public void printKey() {
+        for(int x = 0; x < 4; x++) {
+            for(int y = 0; y < 4; y++) {
+                String temp = Integer.toHexString(keyMatrix[x][y]);
+                if(temp.length() < 2) {
+                    temp = "0" + temp;
+                }
+                System.out.print(temp + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public void printRoundKeys() {
+        for(int x = 0; x < 4; x++) {
+            for(int y = 0; y < 44; y++) {
+                String temp = Integer.toHexString(roundKeyMatrix[x][y]);
+                if(temp.length() < 2) {
+                    temp = "0" + temp;
+                }
+                System.out.print(temp + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    /*Opens files and returns success if scanners and writer opened*/
+    private boolean readFiles() {
+        File keyFile = new File(key);
+        File inputFile = new File(input);
+        outputFile = new File(output);
+
+        try {
+            fileScanner = new Scanner(inputFile);
+            keyScanner = new Scanner(keyFile);
+        }
+        catch(FileNotFoundException e) {
+            System.out.println("Error: Input/Key File Not Found");
+            outputFile.delete();
+            return false;
+        }
+
+        try {
+            outputWriter = new BufferedWriter(new FileWriter(outputFile));
+        }
+        catch(IOException e) {
+            System.out.println("Error: Unable to create output writer");
+            outputFile.delete();
+            return false;
+        }
+        return true;
+    }
+
+    /*Converts matrix into a string following a column major pattern*/
+    private String revertMatrix(int[][] matrix) {
+        String text = "";
+        for(int col = 0; col < matrix[0].length; col++) {
+            for(int row = 0; row < matrix.length; row++) {
+                String temp = Integer.toHexString(matrix[row][col]);
+                if(temp.length() < 2) {
+                    temp = "0" + temp;
+                }
+                text = text + temp;
+            }
+        }
+        text = text + "\n";
+        return text;
+    }
+
+    /*Inserts a given line into plainTextMatrix by column*/
+    private void addToCipherMatrix(String inputLine) {
+        if(inputLine.length() < 32) {
+            int diff = 32 - inputLine.length();
+            String tempPad = "";
+            for(int i = 0; i < diff; i++) {
+                tempPad = tempPad + "0";
+            }
+            inputLine = inputLine + tempPad;
+        }
+
+        if(inputLine.length() > 32) {
+            inputLine = inputLine.substring(0, 32);
+        }
+
+        if(inputLine.length() != 32) {
+            outputFile.delete();
+            assert false; //Panic! Cannot continue if here
+        }
+
+        //Adding elements by column
+        for(int y = 0; y < 4; y++) {
+            for(int x = 0; x < 4; x++) {
+                cipherTextMatrix[x][y] = Integer.parseInt(inputLine.substring(0,2), 16);
+                if(inputLine.length() != 0) {
+                    inputLine = inputLine.substring(2);
+                }
+            }
+        }
+    }
+
+    /*Same as above except for keyMatrix*/
+    private void addToKeyMatrix(String inputLine) {
+        if(inputLine.length() != 32) {
+            outputFile.delete();
+            assert false; //Panic! Cannot continue if here
+        }
+
+
+        //Adding elements by column
+        for(int y = 0; y < 4; y++) {
+            for(int x = 0; x < 4; x++) {
+                keyMatrix[x][y] = Integer.parseInt(inputLine.substring(0,2), 16);
+                if(inputLine.length() != 0) {
+                    inputLine = inputLine.substring(2);
+                }
+
+            }
+        }
+    }
+
+    /*Rotates a given column of the matrix upwards*/
+    private void rotateCol(int col, int[][] matrix) {
+        int temp = matrix[0][col];
+        int row = 1;
+        while(row < matrix.length) {
+            matrix[row - 1][col] = matrix[row++][col];
+        }
+        matrix[row - 1][col] = temp;
+    }
+
+    /*subBytesAux only used for roundkeys here. See inverseSubBytes otherwise*/
+    private void subBytesAux(int y, int[][] matrix) {
+        for(int x = 0; x < matrix.length; x++) {
+            String temp = Integer.toHexString(matrix[x][y]);
+            if(temp.length() != 2) {
+                temp = "0" + temp;
+            }
+            int xVal = Integer.parseInt(temp.substring(0, 1), 16);
+            int yVal = Integer.parseInt(temp.substring(1), 16);
+            matrix[x][y] = Tables.S_BOX[xVal][yVal];
+        }
+
+    }
+
+    /*Calculates all roundkeys and populates the matrix*/
+    private void setRoundKeys() {
+        int row, col;
+        for(col = 0; col < 4; col++) {
+            for(row = 0; row < 4; row++) {
+                roundKeyMatrix[row][col] = keyMatrix[row][col];
+            }
+        }
+        for(; col < roundKeyMatrix[0].length; col++) {
+            if(col % 4 == 0) {
+                for(row = 0; row < 4; row++) {
+                    roundKeyMatrix[row][col] = roundKeyMatrix[row][col - 1];
+                }
+                rotateCol(col, roundKeyMatrix);
+                subBytesAux(col, roundKeyMatrix);
+                int[] rcon = {(Tables.RCON[col/nk]), 0x0, 0x0, 0x0};
+                for(row = 0; row < 4; row++) {
+                    roundKeyMatrix[row][col] = roundKeyMatrix[row][col - 4] ^ roundKeyMatrix[row][col] ^ rcon[row];
+                }
+            }
+            else {
+                for(row = 0; row < 4; row++) {
+                    roundKeyMatrix[row][col] = roundKeyMatrix[row][col - 4] ^ roundKeyMatrix[row][col - 1];
+                }
+
+            }
+        }
+    }
+
+    /*Calls rotateRow for each column*/
+    private void inverseShiftRows() {
+        for(int row = 0; row < cipherTextMatrix.length; row++) {
+            for(int count = 0; count < row; count++) {
+                inverseRotateRow(row, cipherTextMatrix);
+            }
+        }
+    }
+
+    /*Rotates a given row of the matrix to the left*/
+    private void inverseRotateRow(int row, int[][] matrix) {
+        int temp = matrix[row][matrix.length - 1];
+        int col = matrix.length - 2;
+        while(col > 0) {
+            matrix[row][col + 1] = matrix[row][col--];
+        }
+        matrix[row][col] = temp;
+    }
 }
